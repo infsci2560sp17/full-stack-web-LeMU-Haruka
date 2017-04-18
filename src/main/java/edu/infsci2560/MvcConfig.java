b72f53a4c6a2b3601da26014ea857144199d6954
LeMU-Haruka <jingranxie@gmail.com>
2017-04-18 01:16:01 +0000

    1.51

--- src/main/java/edu/infsci2560/MvcConfig.java
+++ src/main/java/edu/infsci2560/MvcConfig.java
@@ -14,6 +14,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         //registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/2017winter").setViewName("2017winter");
+        registry.addViewController("/comingsoon").setViewName("comingsoon");
 
     }
 
